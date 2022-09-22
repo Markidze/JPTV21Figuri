8db66d616c184cb910ed79b8a24922c48ef0071c
pupil <pupil@ivkhk.ee>
2022-09-22 15:16:38 +0300

    Figuri

--- src/jptv21/JPTV21.java
+++ src/jptv21/JPTV21.java
@@ -15,7 +15,31 @@ public class JPTV21 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("=====Фигуры=====");
+            System.out.println("----Фигура_1----");
+
+        for(int j=0; j<10; j++){ 
+        for(int i=0; i<10; i++){System.out.print("* ");
+}       System.out.println();
+
     }
-    
+    System.out.println("----Фигура_2----");
+    System.out.print("Введите длинну: ");
+        int lenght = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Введите ширину: ");
+        int weight = scanner.nextInt();
+        scanner.nextLine();
+        for(int j=1; j<=lenght; j++){  
+            for(int i=1; i<=weight; i++){
+            if(j ==1 || j==lenght || i==1 || i==weight){
+        System.out.print("* ");
+            }
+            else{
+        System.out.print("  ");
+                            }
+            }
+        System.out.println();
+    }
+           
 }
